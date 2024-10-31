23d111106bdf23e5739e23e6836a7423443b0f65
snailuu <huangzhuofeng@idea.edu.cn>
2024-10-31 17:07:22 +0800

    🔧 build: 增加代码规范化

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import viteStylelint from 'vite-plugin-stylelint';
+// @ts-expect-error not type
+import viteEslint from 'vite-plugin-eslint';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/desktop-tools',
+  plugins: [react(),
+
+    viteStylelint({
+      exclude: 'node_modules'
+    }),
+    viteEslint()
+  ],
+  resolve:{
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
